d766e62e4db1877d0296e303fb3bc741683282c7
JohHod <john.hodkinson@softwire.com>
2022-09-13 17:54:58 +0100

    Added photorealistic graphics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,12 @@ class PagesController < ApplicationController
   def index
     api = APIHandler.new()
     params[:postcode] = params[:postcode].upcase
-    @bus_data = api.postcode_bus_info(params[:postcode])
+    debug = false
+    @bus_data = [{"distance"=>91, "common_name"=>"Lady Somerset Road", "stops_with_direction"=>[{"naptan_id"=>"490008660N", "compass_point"=>"N", "towards"=>"Highgate Village Or Parliament Hill Fields", "indicator"=>"GY", "bus_arrivals"=>[{"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"0"}, {"line_id"=>"88", "destination_name"=>"Parliament Hill Fields", "time_to_station"=>"6"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"12"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"15"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"16"}]}, {"naptan_id"=>"490015367S", "compass_point"=>"S", "towards"=>"Camden Town", "indicator"=>"GW", "bus_arrivals"=>[{"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"1"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"3"}, {"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"11"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"12"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"18"}]}]}, {"distance"=>107, "common_name"=>"Greenwood Centre", "stops_with_direction"=>[{"naptan_id"=>"490006943N", "compass_point"=>"N", "towards"=>"Highgate", "indicator"=>"KH", "bus_arrivals"=>[{"line_id"=>"88", "destination_name"=>"Parliament Hill Fields", "time_to_station"=>"7"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"11"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"12"}, {"line_id"=>"214", "destination_name"=>"Highgate Village", "time_to_station"=>"15"}, {"line_id"=>"88", "destination_name"=>"Parliament Hill Fields", "time_to_station"=>"22"}]}, {"naptan_id"=>"490008660S", "compass_point"=>"SE", "towards"=>"Camden Town", "indicator"=>"", "bus_arrivals"=>[{"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"0"}, {"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"1"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"4"}, {"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"12"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"13"}]}]}, {"distance"=>204, "common_name"=>"Fortess Road", "stops_with_direction"=>[{"naptan_id"=>"490006943E", "compass_point"=>"S", "towards"=>"Camden Town", "indicator"=>"KJ", "bus_arrivals"=>[{"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"1"}, {"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"3"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"5"}, {"line_id"=>"88", "destination_name"=>"Clapham Common", "time_to_station"=>"13"}, {"line_id"=>"214", "destination_name"=>"Moorgate, Finsbury Square", "time_to_station"=>"14"}]}]}]
+    if debug == false then
+      @bus_data = api.postcode_bus_info(params[:postcode])
+    end
+
     if @bus_data == [] then params[:error] = 1 end
   end
   class APIHandler
@@ -70,7 +75,7 @@ class PagesController < ApplicationController
                                      "stops_with_direction" => element["children"].map{|child| {"naptan_id"=>child["naptanId"],
                                                                                                 "compass_point"=>if child["additionalProperties"].length>0 then child["additionalProperties"][0]["value"] else "error" end,
                                                                                                 "towards"=>if child["additionalProperties"].length > 1 then child["additionalProperties"][1]["value"] else "error" end,
-                                                                                                "indicator"=>child["indicator"],
+                                                                                                "indicator"=>child["indicator"].length>4?child["indicator"][5..-1]:"",
                                                                                                 "bus_arrivals"=>[]}}}}
       output = output.slice(0,3)
       return output
